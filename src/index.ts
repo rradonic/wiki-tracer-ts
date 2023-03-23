30b6d23757ad12a03e5d74931c22a29501fa0094
Ranko Radonic <ranko.radonic@gmail.com>
2023-03-22 21:29:12 -0700

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const wfp: number = 6;
-console.log("hello world");
+import http from "http";
+
+const hostname = "127.0.0.1";
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
