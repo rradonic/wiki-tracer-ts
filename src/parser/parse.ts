5ed52d0d6399b79a378e786d7461582c47020903
Ranko Radonic <ranko.radonic@gmail.com>
2024-09-09 18:33:28 +0000

    WIP

--- src/parser/parse.ts
+++ src/parser/parse.ts
@@ -7,28 +7,20 @@ if (process.argv.length < 3) {
   process.exit();
 }
 
-// using then() because there's no top level await yet in node's module system
-prisma.link
-  .deleteMany({})
-  .then(() => {
-    return prisma.page.deleteMany({});
-  })
-  .then(() => {
-    const readStream = fs.createReadStream(process.argv[2], {
-      highWaterMark: 1024 * 1024,
-    });
+const readStream = fs.createReadStream(process.argv[2], {
+  highWaterMark: 1024 * 1024,
+});
 
-    const parser = new Parser();
+const parser = new Parser();
 
-    type Stringable = {
-      toString(): string;
-    };
+type Stringable = {
+  toString(): string;
+};
 
-    readStream.on("data", (chunk: Stringable) => {
-      if (parser.counter > 100) {
-        readStream.destroy();
-      }
+readStream.on("data", (chunk: Stringable) => {
+  if (parser.counter > 100) {
+    readStream.destroy();
+  }
 
-      parser.expat.write(chunk.toString());
-    });
-  });
+  parser.expat.write(chunk.toString());
+});
