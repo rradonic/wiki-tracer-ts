4a1ef15892eff8675834db55c8868cff9f68b068
Ranko Radonic <ranko.radonic@gmail.com>
2024-10-25 13:43:57 +0000

    Move starting page to root URL

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="font-bold bg-slate-100 max-w-sm mx-auto p-6 rounded-lg shadow-md mt-6">
+      <h1 className="text-slate-700">Wikipedia Tracer</h1>
+    </div>
+  );
+}
